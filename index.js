914655d1dfa70c142598951e4619c033c583338b
StanislavKaisin <8050361@gmail.com>
2020-10-14 11:28:20 +0300

    Open Close Principle

--- index.js
+++ index.js
@@ -82,3 +82,87 @@ console.log("printer.json()=", printer.json());
 
 // Open Close Principle
 console.log("Open Close Principle");
+
+class Shape {
+  area() {
+    throw new Error("Area method should be implemented");
+  }
+}
+
+class Square extends Shape {
+  constructor(size) {
+    super();
+    // this.type = "square";
+    this.size = size;
+  }
+  area() {
+    return this.size ** 2;
+  }
+}
+
+class Circle extends Shape {
+  constructor(radius) {
+    super();
+    // this.type = "circle";
+    this.radius = radius;
+  }
+  area() {
+    return this.radius ** 2 * Math.PI;
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super();
+    // this.type = "rectangle";
+    this.width = width;
+    this.height = height;
+  }
+  area() {
+    return this.width * this.height;
+  }
+}
+
+class RectTriangle extends Shape {
+  constructor(a, b) {
+    super();
+    this.a = a;
+    this.b = b;
+  }
+  area() {
+    return 0.5 * this.a * this.b;
+  }
+}
+
+const circle = new Circle(5);
+console.log("circle=", circle);
+
+class AreaCalcuator {
+  constructor(shapes = []) {
+    this.shapes = shapes;
+  }
+  sum() {
+    return this.shapes.reduce((acc, shape) => {
+      //wrong!!!
+      // if (shape.type === "circle") {
+      //   acc += shape.radius ** 2 * Math.PI;
+      // } else if (shape.type === "square") {
+      //   acc += shape.size ** 2;
+      // } else if (shape.type === "rectangle") {
+      //   acc += shape.width * shape.height;
+      // }
+      acc += shape.area();
+      return acc;
+    }, 0);
+  }
+}
+
+const calc = new AreaCalcuator([
+  new Square(10),
+  new Circle(1),
+  new Circle(5),
+  new Rectangle(10, 1),
+  new RectTriangle(30, 40),
+]);
+
+console.log("calc.sum()=", calc.sum());
